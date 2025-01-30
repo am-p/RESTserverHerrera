74f325baf4eae444899e534c124a60a1a5b4ca87
Ariel Pardo <ariel.marcelo.pardo@accenture.com>
2025-01-29 21:40:19 -0300

    Finished task lesson 133

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -14,14 +14,7 @@ const usuariosGet = (req = request, res = response) => {
 const usuariosPost = async(req, res = response) => {
     
     const { nombre, correo, password, rol } = req.body;
-    const usuario = new Usuario({ nombre, correo, password, rol});
-    const existeEmail = await Usuario.findOne({ correo });
-
-    if ( existeEmail) {
-	return res.status(400).json({
-	    msg: 'Ese correo ya se registro'
-	});
-    }
+    const usuario = new Usuario({ nombre, correo, password, rol});   
     const salt = bcryptjs.genSaltSync();
     usuario.password = bcryptjs.hashSync(password, salt);
     await usuario.save();
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,7 +2,7 @@ const { Router } = require('express'); //es una funcion de express
 const { usuariosGet, usuariosPut, usuariosPost, usuariosPatch, usuariosDelete } = require('../controllers/usuarios');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { esRoleValido } = require('../helpers/db-validators');
+const { esRoleValido, emailExiste } = require('../helpers/db-validators');
 
 
 const router = Router();
@@ -11,7 +11,7 @@ router.get('/', usuariosGet);
 router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser de mas 6 letras').isLength({ min:6 }),
-    check('correo', 'El correo no es valido').isEmail(),
+    check('correo').custom(emailExiste),
     check('rol').custom(esRoleValido), //rol se le envía al esRoleValido, si no se vé es porque es redundante, cosas de las funciones flecha, I guess
     validarCampos
 ], usuariosPost);
