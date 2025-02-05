a53509bdf469904f3661eedebeefd34cfcf3851b
Ariel Pardo <ariel.marcelo.pardo@accenture.com>
2025-02-05 08:04:41 -0300

    Starting lesson 184: products cRUD

--- models/server.js
+++ models/server.js
@@ -12,6 +12,7 @@ class Server {
 	this.paths = {
 	    auth:       '/api/auth',
 	    usuarios:   '/api/usuarios',
+	    productos:  '/api/productos',
 	    categorias: '/api/categorias'
 	}
 	this.conectarDB();
@@ -33,6 +34,7 @@ class Server {
 	this.app.use(this.paths.auth, require('../routes/auth'));
 	this.app.use(this.paths.usuarios, require('../routes/usuarios'));
 	this.app.use(this.paths.categorias, require('../routes/categorias'));
+	this.app.use(this.paths.productos, require('../routes/productos'));
     }
 
     listen() {
