43962ed0d9baab5417bee54cb55a66992867f150
Ariel Pardo <ariel.marcelo.pardo@accenture.com>
2025-02-05 09:11:39 -0300

    Starting lesson 184: products CRUD before laptop change

--- controllers/productos.js
+++ controllers/productos.js
@@ -1,1 +1,28 @@
-const 
+const { response } = require("express");
+const { Producto } = require("../models");
+
+const crearProducto = async(req, res = response) => {
+    const nombre = req.body.nombre.toUpperCase();
+    const productoDB = await Producto.findOne({ nombre });
+
+    if (productoDB) {
+	return res.status(400).json({
+	    msg: `El producto ${ productoDB.nombre } ya existe`
+	})
+    }
+     
+    const data = {
+	nombre,
+	usuario: req.usuario._id,
+	categoria: req.categoria._id
+    };
+
+    const producto = new Producto(data);
+    await producto.save();
+
+    res.status(201).json(categoria);
+}
+
+module.exports = {
+    crearProducto
+}
