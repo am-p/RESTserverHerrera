5960e26e83f3600c3a62622059de87bb14df82b9
ariel.marcelo.pardo <ariel.marcelo.pardo@accenture.com>
2025-02-07 19:11:06 -0300

    Finished lesson 198

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,5 @@
 const { request, response } = require("express");
-const path = require('path');
+const { subirArchivo } = require("../helpers");
 
 const cargarArchivo = async(req = request, res = response) => {
 
@@ -10,26 +10,10 @@ const cargarArchivo = async(req = request, res = response) => {
 	return;
     };
 
-    const { archivo } = req.files;
-    const nombreCortado = archivo.name.split('.');
-    const extension = nombreCortado[ nombreCortado.length -1 ];
+    const nombre = await subirArchivo(req.files);
 
-    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
-    if (!extensionesValidas.includes(extension)) {
-	return res.status(400).json({
-	    msg: `La extension ${ extension } no es la permitida ${ extensionesValidas }`
-	});
-    }
-    res.json({ extension });
-
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
-
-    archivo.mv(uploadPath, (err) => {
-	if (err) {
-	    return res.status(500).json({ err });
-	}
-
-	res.json({ msg: 'File uploaded to ' + uploadPath });
+    res.json({
+	nombre
     });
 };
 
