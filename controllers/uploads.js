f202de92466835a4b62e46c3fc1c0dc651e89a2c
ariel.marcelo.pardo <ariel.marcelo.pardo@accenture.com>
2025-02-08 17:50:22 -0300

    Finished lesson 201

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,15 +1,9 @@
 const { request, response } = require("express");
 const { subirArchivo } = require("../helpers");
+const { Usuario, Producto } = require('../models');
 
 const cargarArchivo = async(req = request, res = response) => {
 
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-	res.status(400).json({
-	    msg: 'No hay archivos para subir'
-	});
-	return;
-    };
-    
     try {
 	//const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
 	const nombre = await subirArchivo(req.files, undefined, 'imgs');
@@ -20,6 +14,40 @@ const cargarArchivo = async(req = request, res = response) => {
     
 };
 
+const actualizarImagen = async(req = request, res = response) => {
+
+    const { id, coleccion } = req.params;
+    let modelo;
+
+    switch (coleccion) {
+    case 'usuarios':
+	modelo = await Usuario.findById(id);
+	if (!modelo) {
+	    return res.status(400).json({
+		msg: `No existe un usuario con el id ${id}`
+	    });
+	}
+	break;
+    case 'productos':
+	modelo = await Producto.findById(id);
+	if (!modelo) {
+	    return res.status(400).json({
+		msg: `No existe un producto con el id ${id}`
+	    });
+	}
+	break;
+    default:
+	return res.status(500).json({ msg: 'Se me olvido validar esto'});
+    }
+    
+    const nombre = await subirArchivo(req.files, undefined, coleccion);
+    modelo.img = nombre;
+    await modelo.save();
+    
+    res.json(modelo);
+};
+
 module.exports = {
-    cargarArchivo
+    cargarArchivo,
+    actualizarImagen
 }
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validaCampos = require('../middlewares/validar-campos');
 const validarJWT = require('../middlewares/validar-jwt');
 const tieneRole = require('../middlewares/validar-roles');
+const validarArchivo = require('../middlewares/validar-archivo');
 
 module.exports = {
     ...validaCampos,
     ...validarJWT,
     ...tieneRole,
+    ...validarArchivo
 }
