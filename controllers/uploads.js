5da2ff0124d934025f85d3f39697234375ffb5b9
ariel.marcelo.pardo <ariel.marcelo.pardo@accenture.com>
2025-02-08 16:08:14 -0300

    Finished lesson 199

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -9,12 +9,15 @@ const cargarArchivo = async(req = request, res = response) => {
 	});
 	return;
     };
-
-    const nombre = await subirArchivo(req.files);
-
-    res.json({
-	nombre
-    });
+    
+    try {
+	//const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
+	const nombre = await subirArchivo(req.files, undefined, 'imgs');
+	res.json( {nombre} )
+    } catch(msg) {
+	res.status(400).json({msg});
+    }
+    
 };
 
 module.exports = {
--- models/server.js
+++ models/server.js
@@ -33,7 +33,8 @@ class Server {
 	this.app.use(express.static('public'));
 	this.app.use(fileUpload({
 	    useTempFiles : true,
-	    tempFileDir : '/tmp/'
+	    tempFileDir : '/tmp/',
+	    createParentPath: true
 	}));
     }
 
